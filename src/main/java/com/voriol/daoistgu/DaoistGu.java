8f4fdc07898696881aecd98348ea5e08226e6a32
Voriol <nikita.kovin12@gmail.com>
2026-03-07 01:46:48 +0500

    Followed tutorial and added some directories as well as first ingredient item - bismuth. Check out hand-made texture as well

--- src/main/java/com/voriol/daoistgu/DaoistGu.java
+++ src/main/java/com/voriol/daoistgu/DaoistGu.java
@@ -1,5 +1,8 @@
 package com.voriol.daoistgu;
 
+import com.voriol.daoistgu.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -33,6 +36,8 @@ public class DaoistGu {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -45,7 +50,9 @@ public class DaoistGu {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/voriol/daoistgu/item/ModItems.java
@@ -0,0 +1,18 @@
+package com.voriol.daoistgu.item;
+
+import com.voriol.daoistgu.DaoistGu;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DaoistGu.MOD_ID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
