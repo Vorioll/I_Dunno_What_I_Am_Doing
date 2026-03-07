8438da482e11aba8d01eb50c59d85f0ea0114af0
Kukumruh <126451476+Kukumruh@users.noreply.github.com>
2026-03-08 01:07:19 +0500

    HAHAHahahhAhSA

--- src/main/java/com/voriol/daoistgu/item/Primal_Stone.java
+++ src/main/java/com/voriol/daoistgu/item/Primal_Stone.java
@@ -1,14 +1,24 @@
-//public class Primal_Stone() extends Item {
-//    public Primal_Stone(Properties properties) {
-//        super(properties);
-//    }
-//    @Override
-//    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-//        ItemStack itemStack = player.getItemInHand(usedHand);
-//        if (!level.isClientSide) {
-//            player.sendSystemMessage(Component.literal("Сосать Америка"));
-//        }
-//        itemStack.hurtAndShrink(1, player);
-//        return InteractionResultHolder.consume(itemStack);
-//    }
-//}
+package com.voriol.daoistgu.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+public class Primal_Stone extends Item {
+    public Primal_Stone(Properties properties) {
+        super(properties);
+    }
+    @Override
+    @NotNull
+    public InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand usedHand) {
+        ItemStack itemStack = player.getItemInHand(usedHand);
+        if (!player.getAbilities().instabuild) {
+            itemStack.shrink(1);
+        }
+        return InteractionResultHolder.consume(itemStack);
+    }
+}
