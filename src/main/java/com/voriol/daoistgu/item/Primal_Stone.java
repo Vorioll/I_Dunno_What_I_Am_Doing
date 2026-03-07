af141b7ae4facad952ba34b5f17834492f566678
Voriol <nikita.kovin12@gmail.com>
2026-03-08 00:26:39 +0500

    убрал в комменты новый код и исправил ошибку New ITEM

--- src/main/java/com/voriol/daoistgu/item/Primal_Stone.java
+++ src/main/java/com/voriol/daoistgu/item/Primal_Stone.java
@@ -1,14 +1,14 @@
-public class Primal_Stone() extends Item {
-    public Primal_Stone(Properties properties) {
-        super(properties);
-    }
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        ItemStack itemStack = player.getItemInHand(usedHand);
-        if (!level.isClientSide) {
-            player.sendSystemMessage(Component.literal("Сосать Америка"));
-        }
-        itemStack.hurtAndShrink(1, player);
-        return InteractionResultHolder.consume(itemStack);
-    }
-}
+//public class Primal_Stone() extends Item {
+//    public Primal_Stone(Properties properties) {
+//        super(properties);
+//    }
+//    @Override
+//    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+//        ItemStack itemStack = player.getItemInHand(usedHand);
+//        if (!level.isClientSide) {
+//            player.sendSystemMessage(Component.literal("Сосать Америка"));
+//        }
+//        itemStack.hurtAndShrink(1, player);
+//        return InteractionResultHolder.consume(itemStack);
+//    }
+//}
